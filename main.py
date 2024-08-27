52f70ef7642f213a46621d0a8919f337cb1296ba
Hritik rawat <148574633+hritik013@users.noreply.github.com>
2024-08-27 19:45:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import string
+from collections import Counter
+import matplotlib.pyplot as plt
+text = open('read.txt', encoding='utf-8').read()
+lowercase = text.lower()
+remov_pun = lowercase.translate(str.maketrans(' ', ' ', string.punctuation))
+token_words = remov_pun.split()
+useless_words = [
+    "a", "about", "above", "after", "again", "against", "all", "am", "an", "and",
+    "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being",
+    "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't",
+    "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during",
+    "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have",
+    "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's",
+    "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll",
+    "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself",
+    "let's", "me", "more", "most", "mustn't", "my", "myself", "nor",
+    "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours",
+    "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll",
+    "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's",
+    "the", "their", "theirs", "them", "themselves", "then", "there", "there's",
+    "these", "they", "they'd", "they'll", "they're", "they've", "this", "those",
+    "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we",
+    "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when",
+    "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why",
+    "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're",
+    "you've", "your", "yours", "yourself", "yourselves"
+]
+final = []
+for word in token_words:
+    if word not in useless_words:
+        final.append(word)
+print(final)
+empty_emotion_list = []
+with open('sentiments.txt', 'r') as file:
+    for line in file:
+        clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+        word, emotion = clear_line.split(':')
+
+        if word in final:
+            empty_emotion_list.append(emotion)
+
+
+
+print(empty_emotion_list)
+
+a = Counter(empty_emotion_list)
+print(a)
+fig, axis = plt.subplots()
+axis.bar(a.keys(), a.values())
+fig.autofmt_xdate()
+plt.savefig('Mygraph.png')
+plt.show()
